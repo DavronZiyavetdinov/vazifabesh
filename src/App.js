8e4058ddc823d81b21a58bd1759a34022e89522c
DavronZiyavetdinov <sultonbekmirtolipov025@gmail.com>
2024-03-05 17:01:10 +0300

    add rasm

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
+import monitor from './monitor.jpg'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello world </h1>
+      <h1>Hello world1 </h1>
       <header className="App-header">
+        <img src={monitor} alt="" />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
