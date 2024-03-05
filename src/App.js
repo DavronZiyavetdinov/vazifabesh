9579ead2ca0026d50fc2fe2648855caab0c01d7c
DavronZiyavetdinov <sultonbekmirtolipov025@gmail.com>
2024-03-05 16:55:09 +0300

    hello

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <h1>Assalou Aleykum</h1>
       <header>
-        
+
       </header>
     </div>
   );
