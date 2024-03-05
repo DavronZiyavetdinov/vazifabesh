ee5f2fcd7cbcee5e4a86eaa733c9dc97e4690a37
DavronZiyavetdinov <sultonbekmirtolipov025@gmail.com>
2024-03-05 17:03:32 +0300

    add chopildi

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import monitor from './monitor.jpg'
 import './App.css';
 
@@ -6,21 +5,8 @@ function App() {
   return (
     <div className="App">
       <h1>Hello world1 </h1>
-      <header className="App-header">
-        <img src={monitor} alt="" />
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img src={monitor} alt="" />
+      
     </div>
   );
 }
